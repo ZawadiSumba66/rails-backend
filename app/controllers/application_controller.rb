c2b7e25e861e996639bb6c1e12afce433fb08c05
ZawadiSumba66 <rosesumba98@gmail.com>
2021-08-07 19:30:35 +0300

    Modify to include unpermitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:user, keys: [:image])
+  end
 end
