0524c18c7c0bc2f1099f574d0b9eef4cd1e5058f
ZawadiSumba66 <rosesumba98@gmail.com>
2021-08-07 19:32:02 +0300

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post :auth, to: 'authentication#create'
+      resources :users
+      resources :favorites
+      resources :tips do
+        post 'favorite', to: 'tips#favorite'
+        post 'unfavourite', to: 'tips#unfavourite'
+      end
+    end
+ end
 end
