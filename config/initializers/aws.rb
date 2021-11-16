543938f47a8c4f19036dc8e5ca8a14f533cf368d
ZawadiSumba66 <rosesumba98@gmail.com>
2021-11-16 19:04:13 +0300

    s3 region

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,7 @@
 require 'aws-sdk-s3'
 
 Aws.config.update({
-  region: ENV['AWS_REGION'],
+  s3_region: ENV['AWS_REGION'],
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
 })
 
